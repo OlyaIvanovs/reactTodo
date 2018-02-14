d367f882c3c26bc44f2a991f1a89eb7affb66958
Olya Ivanova <olya@ivanovs.info>
2018-02-14 23:56:39 +1300

    First commit

---
+++ src/app/index.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var createReactClass = require('create-react-class');
+
+var ToDoComponent = createReactClass({
+    render: function() {
+        return(
+            <h1>Hgdfgodfjglk</h1>
+        )
+    }
+})
+
+ReactDOM.render(<ToDoComponent />, document.getElementById('todo-wrapper'))
