ede77db257cfc513902041e6ce3316423c7f60fa
Olya Ivanova <olya@ivanovs.info>
2018-02-15 23:16:31 +1300

    Added components for todolist

--- src/app/index.js
+++ src/app/index.js
@@ -2,12 +2,51 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var createReactClass = require('create-react-class');
 
-var ToDoComponent = createReactClass({
-    render: function() {
-        return(
-            <h1>Hgdfgodfjglk</h1>
+class TodoItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <li>
+                <div className="todo-item">
+                    <span className="todo-name">{this.props.item}</span>
+                </div>
+            </li>
         )
     }
-})
+}
 
-ReactDOM.render(<ToDoComponent />, document.getElementById('todo-wrapper'))
+class ToDoComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: ['eat', 'sleep', 'pray']
+        }
+    }
+
+    render() {
+        var todos = this.state.todos.map((todo, index) => {
+            return (
+                // <li key={index}>{todo}</li>
+                <TodoItem item={todo} key={index}/>
+            )
+        })
+        return (
+            <div>
+                <h1>
+                    You have to do
+                </h1>
+                <ul>{todos}</ul>
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(
+    <ToDoComponent />, 
+    document.getElementById('todo-wrapper')
+)
